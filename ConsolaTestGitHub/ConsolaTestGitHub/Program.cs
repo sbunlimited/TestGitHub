0ad0ef7ea87e7ea44064201645206f3b789500fd
salvabasile <salvabasile@hotmail.com>
2015-07-29 20:53:59 -0300

    Inicializacion de repositorio
    Aplicacion de consola

---
+++ ConsolaTestGitHub/ConsolaTestGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolaTestGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Probando githu");
+            Console.ReadLine();
+        }
+    }
+}

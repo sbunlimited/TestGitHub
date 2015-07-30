aabbe87962ffc602c1961f03c41610c01ffb2fdd
salvabasile <salvabasile@hotmail.com>
2015-07-29 21:17:27 -0300

    Clonar repo

--- ConsolaTestGitHub/ConsolaTestGitHub/Program.cs
+++ ConsolaTestGitHub/ConsolaTestGitHub/Program.cs
@@ -16,6 +16,8 @@ namespace ConsolaTestGitHub
             Console.ReadLine();
             Console.WriteLine("Probando nuevo commit desde Visual Studio");
             Console.ReadLine();
+            Console.WriteLine("Probando proyecto en comun. Esto es clonar un repositorio online");
+            Console.ReadLine();
         }
     }
 }
